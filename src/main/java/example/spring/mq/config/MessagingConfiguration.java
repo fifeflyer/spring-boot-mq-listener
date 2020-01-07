3b13a3aed92e9eafb28d4ae582e8495c74efa015
Wayne Thomson <waynet@btinternet.com>
2020-01-07 21:24:41 +0000

    Updates to override Spring Boot default behaviour for JMS Listener

--- src/main/java/example/spring/mq/config/MessagingConfiguration.java
+++ src/main/java/example/spring/mq/config/MessagingConfiguration.java
@@ -1,11 +1,21 @@
 package example.spring.mq.config;
 
+import com.ibm.mq.jms.MQQueue;
+import example.spring.mq.message.EventListener;
+import example.spring.mq.message.MessagingErrorHandler;
+import example.spring.mq.message.MessagingProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.jms.listener.DefaultMessageListenerContainer;
+
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
 
 @Configuration
 public class MessagingConfiguration {
 
-// Enabling this prevents messages being placed on the backout queue.
+// Enabling this prevents messages being placed on the backout queue (needs sessionTransacted set to true).
 //    @Bean
 //    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(
 //        ConnectionFactory connectionFactory,
@@ -16,4 +26,32 @@ public class MessagingConfiguration {
 //        factory.setErrorHandler(errorHandler);
 //        return factory;
 //    }
+
+    // Enabling this allows a listener implementing SessionAwareMessageListener<TextMessage>
+    // to be picked up by Spring Boot.  However, such listeners don't place failed messages
+    // onto the backout queue unless transacted = true!
+    //
+    // MS Client will put poison messages to the backout queue defined on the queue's BackoutRequeueQueue
+    // (BOQNAME) after the number of roll backs exceeds that defined in the BackoutThreshold (BOTHRESH).
+    // This means that the user running the JMS application not only needs access to the application queue,
+    // it also needs access to put to the named backout queue too.
+    //
+    // If the backout queue cannot be accessed or is not defined, the queue manager's dead letter queue
+    // (DEADQ attribute) will attempt to be used.
+    @Bean
+    public DefaultMessageListenerContainer defaultMessageListenerContainer(
+        ConnectionFactory connectionFactory,
+        EventListener eventListener,
+        MessagingErrorHandler errorHandler,
+        MessagingProperties messagingProperties) throws JMSException {
+
+        DefaultMessageListenerContainer container = new DefaultMessageListenerContainer();
+        container.setMessageListener(eventListener);
+        container.setConnectionFactory(connectionFactory);
+        container.setDestination(new MQQueue(messagingProperties.getQueue()));
+        container.setSessionTransacted(true); // Required for backout to work!
+        container.setErrorHandler(errorHandler);
+        container.setTaskExecutor(new SimpleAsyncTaskExecutor("events")); // Rename thread!
+        return container;
+    }
 }
--- src/main/java/example/spring/mq/message/EventListener.java
+++ src/main/java/example/spring/mq/message/EventListener.java
@@ -5,32 +5,49 @@ import example.spring.mq.model.Event;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.listener.SessionAwareMessageListener;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.Session;
 import javax.jms.TextMessage;
 import java.io.IOException;
 
 @Component
 @AllArgsConstructor
 @Slf4j
-public class EventListener {
+//public class EventListener {
+//
+//    private final ObjectMapper objectMapper;
+//
+//    @JmsListener(destination = "ORGANISATION.EVENTS.BOS.A")
+//    public void onMessage(Message message) {
+//        if (message instanceof TextMessage) {
+//            try {
+//                String msg = ((TextMessage) message).getText();
+//                Event event = objectMapper.readValue(msg, Event.class);
+//                log.info("Message type [{}]", event.getEventType());
+//            } catch (JMSException | IOException e) {
+//                throw new RuntimeException(e);
+//            }
+//        } else {
+//            throw new IllegalArgumentException("Message Error");
+//        }
+//    }
+//}
+public class EventListener implements SessionAwareMessageListener<TextMessage> {
 
     private final ObjectMapper objectMapper;
 
-    @JmsListener(destination = "ORGANISATION.EVENTS.BOS.A")
-    public void onMessage(Message message) {
-        if (message instanceof TextMessage) {
-            try {
-                String msg = ((TextMessage) message).getText();
+    @Override
+    public void onMessage(TextMessage textMessage, Session session) {
+        try {
+                String msg = textMessage.getText();
                 Event event = objectMapper.readValue(msg, Event.class);
                 log.info("Message type [{}]", event.getEventType());
             } catch (JMSException | IOException e) {
                 throw new RuntimeException(e);
             }
-        } else {
-            throw new IllegalArgumentException("Message Error");
-        }
     }
 }
